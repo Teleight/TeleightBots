76cc20cd454d2f2fa1177dc142060d005b27665a
firo <56583784+literallyfiro@users.noreply.github.com>
2023-11-13 19:00:33 +0100

    Create Game

---
+++ src/main/java/org/teleight/teleightbots/api/objects/Game.java
@@ -0,0 +1,30 @@
+package org.teleight.teleightbots.api.objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.jetbrains.annotations.Nullable;
+import org.teleight.teleightbots.api.ApiResult;
+import org.teleight.teleightbots.api.objects.entities.MessageEntity;
+
+public record Game(
+        @JsonProperty(value = "title", required = true)
+        String title,
+
+        @JsonProperty(value = "description", required = true)
+        String description,
+
+        @JsonProperty(value = "photo", required = true)
+        PhotoSize[] photo,
+
+        @JsonProperty("text")
+        @Nullable
+        String text,
+
+        @JsonProperty("text_entities")
+        @Nullable
+        MessageEntity[] textEntities,
+
+        @JsonProperty("animation")
+        @Nullable
+        Animation animation
+) implements ApiResult {
+}
