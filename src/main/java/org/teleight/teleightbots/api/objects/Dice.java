9f0216236b26800fd4827fc04bd17e6b94a44b2b
firo <56583784+literallyfiro@users.noreply.github.com>
2023-11-13 18:58:37 +0100

    Create Dice

---
+++ src/main/java/org/teleight/teleightbots/api/objects/Dice.java
@@ -0,0 +1,13 @@
+package org.teleight.teleightbots.api.objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.teleight.teleightbots.api.ApiResult;
+
+public record Dice(
+        @JsonProperty(value = "emoji", required = true)
+        String emoji,
+
+        @JsonProperty(value = "value", required = true)
+        Integer value
+) implements ApiResult {
+}
