0f6e4f7dae713df401f14eeb3914144bf9cafc88
Stredox02 <29194230+Stredox02@users.noreply.github.com>
2023-12-05 11:38:08 +0100

    Added DeleteMessage method

---
+++ src/main/java/org/teleight/teleightbots/api/methods/updating/DeleteMessage.java
@@ -0,0 +1,62 @@
+package org.teleight.teleightbots.api.methods.updating;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.jetbrains.annotations.NotNull;
+import org.teleight.teleightbots.api.ApiMethodBoolean;
+
+public record DeleteMessage(
+        @JsonProperty(value = "chat_id", required = true)
+        String chatID,
+
+        @JsonProperty(value = "message_id",required = true)
+        int messageId
+) implements ApiMethodBoolean {
+
+    public static @NotNull Builder builder() {
+        return new BuilderImpl();
+    }
+
+    @Override
+    public @NotNull String getEndpointURL() {
+        return "deleteMessage";
+    }
+
+    public sealed interface Builder permits BuilderImpl {
+        @NotNull Builder chatId(String chatId);
+
+        @NotNull Builder chatId(int chatId);
+
+        @NotNull Builder messageId(int messageId);
+
+        @NotNull DeleteMessage build();
+    }
+
+    static final class BuilderImpl implements Builder {
+        private String chatId;
+        private int messageId;
+
+        @Override
+        public @NotNull Builder chatId(String chatId) {
+            this.chatId = chatId;
+            return this;
+        }
+
+        @Override
+        public @NotNull Builder chatId(int chatId) {
+            this.chatId = "" + chatId;
+            return this;
+        }
+
+        @Override
+        public @NotNull Builder messageId(int messageId) {
+            this.messageId = messageId;
+            return this;
+        }
+
+        @Override
+        public @NotNull DeleteMessage build() {
+            return new DeleteMessage(chatId, messageId);
+        }
+    }
+
+}
