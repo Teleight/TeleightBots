c3ee1a4d9ba54c1bbee8c168e068550c63ce341b
firo <56583784+literallyfiro@users.noreply.github.com>
2024-02-23 18:04:49 +0100

    Add extension docs

--- src/main/java/org/teleight/teleightbots/extensions/ExtensionManager.java
+++ src/main/java/org/teleight/teleightbots/extensions/ExtensionManager.java
@@ -5,14 +5,31 @@ import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.Set;
 
+/**
+ * Base interface for the extension manager.
+ *
+ * @see ExtensionManagerImpl for the default implementation.
+ */
 public interface ExtensionManager {
 
+    // The name of the extension blueprint file.
     String EXTENSION_FILE = "teleight-extension.json";
 
+    /**
+     * Starts the extension manager.
+     */
     void start();
 
+    /**
+     * Safely shut down the extension manager.
+     */
     void shutdown();
 
+    /**
+     * Returns a set of the loaded extensions.
+     *
+     * @return An unmodifiable set of the loaded extensions.
+     */
     @NotNull @Unmodifiable Set<Extension> getLoadedExtensions();
 
 }
