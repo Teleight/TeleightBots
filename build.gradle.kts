de9792e096c073e6f1cb44266b8fcd8359f961c9
Stredox02 <29194230+Stredox02@users.noreply.github.com>
2024-02-24 18:30:48 +0100

    Bump libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,16 +19,16 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
+    //Json
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+
     //Checker
-    implementation("org.checkerframework:checker:3.39.0")
-    implementation("org.checkerframework:checker-qual:3.39.0")
+    implementation("org.checkerframework:checker:3.42.0")
+    implementation("org.checkerframework:checker-qual:3.42.0")
 
     //Lombok
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
-
-    //Json
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
 
     //Annotations
     implementation("org.jetbrains:annotations:24.0.1")
