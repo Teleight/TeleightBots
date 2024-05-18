6e95dd5dac0d9b2085de0e054c12fd8d6a708748
firo <56583784+literallyfiro@users.noreply.github.com>
2024-05-18 18:19:52 +0200

    Add null-checking when getting `Chat` from `GroupBotEvent`

--- src/main/java/org/teleight/teleightbots/event/trait/GroupBotEvent.java
+++ src/main/java/org/teleight/teleightbots/event/trait/GroupBotEvent.java
@@ -23,11 +23,16 @@ public interface GroupBotEvent extends Event {
      * @return the Chat object associated with the event
      */
     default Chat chat() {
-        if (update().myChatMember() != null) {
-            return update().myChatMember().chat();
+        final ChatMemberUpdated myChatMember = update().myChatMember();
+        if (myChatMember != null) {
+            return myChatMember.chat();
         } else {
-            return update().message().chat();
+            final Message message = update().message();
+            if (message != null) {
+                return message.chat();
+            }
         }
+        return null;
     }
 
 }
