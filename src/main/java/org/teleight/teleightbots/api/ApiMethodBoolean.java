0f2e504c66317046a8fc9463bea838927796edaf
firo <56583784+literallyfiro@users.noreply.github.com>
2024-05-18 17:50:29 +0200

    Cleanup `ApiMethodBoolean` and `ApiMethodMessage`

--- src/main/java/org/teleight/teleightbots/api/ApiMethodBoolean.java
+++ src/main/java/org/teleight/teleightbots/api/ApiMethodBoolean.java
@@ -1,15 +1,8 @@
 package org.teleight.teleightbots.api;
 
-import org.jetbrains.annotations.NotNull;
-import org.teleight.teleightbots.exception.exceptions.TelegramRequestException;
-
 /**
  * Represents a method that returns a boolean result.
  */
 public interface ApiMethodBoolean extends ApiMethod<Boolean> {
-
-    default @NotNull Boolean deserializeResponse(@NotNull String answer) throws TelegramRequestException {
-        return deserializeResponse(answer, Boolean.class);
-    }
 
 }
--- src/main/java/org/teleight/teleightbots/api/ApiMethodMessage.java
+++ src/main/java/org/teleight/teleightbots/api/ApiMethodMessage.java
@@ -1,16 +1,9 @@
 package org.teleight.teleightbots.api;
 
-import org.jetbrains.annotations.NotNull;
 import org.teleight.teleightbots.api.objects.Message;
-import org.teleight.teleightbots.exception.exceptions.TelegramRequestException;
 
 /**
  * Represents a method that returns a message result.
  */
 public interface ApiMethodMessage extends ApiMethod<Message> {
-
-    default @NotNull Message deserializeResponse(@NotNull String answer) throws TelegramRequestException {
-        return deserializeResponse(answer, Message.class);
-    }
-
 }
