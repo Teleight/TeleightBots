cc0ec1cbc182951f9235600dedb17560ce8d4f70
firo <56583784+literallyfiro@users.noreply.github.com>
2024-05-19 23:23:15 +0200

    Heavily optimize event manager implementation

--- src/main/java/org/teleight/teleightbots/event/EventManagerImpl.java
+++ src/main/java/org/teleight/teleightbots/event/EventManagerImpl.java
@@ -4,18 +4,21 @@ package org.teleight.teleightbots.event;
 import org.jetbrains.annotations.NotNull;
 import org.teleight.teleightbots.event.trait.Event;
 
-import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Consumer;
 
 public class EventManagerImpl implements EventManager {
 
-    private final List<EventListener<? extends Event>> registeredEvents = new CopyOnWriteArrayList<>();
+    private final Map<Class<? extends Event>, Queue<EventListener<? extends Event>>> listeners = new ConcurrentHashMap<>();
 
     @Override
     public <T extends Event> @NotNull EventManager addListener(@NotNull EventListener<T> listener) {
-        this.registeredEvents.add(listener);
+        listeners.computeIfAbsent(listener.eventType(), k -> new ConcurrentLinkedQueue<>()).add(listener);
         return this;
     }
 
@@ -26,18 +29,21 @@ public class EventManagerImpl implements EventManager {
 
     @Override
     public <T extends Event> @NotNull CompletableFuture<T> call(@NotNull T event) {
+        Queue<EventListener<? extends Event>> eventListeners = listeners.get(event.getClass());
+        if (eventListeners == null || eventListeners.isEmpty()) {
+            return CompletableFuture.completedFuture(event);
+        }
         return CompletableFuture.supplyAsync(() -> {
-            for (EventListener<? extends Event> registeredEvent : registeredEvents) {
-                if (registeredEvent.eventType().isInstance(event)) {
-                    @SuppressWarnings("unchecked") final EventListener<T> listener = (EventListener<T>) registeredEvent;
-                    final EventListener.Result result = listener.run(event);
-                    if (result == EventListener.Result.EXPIRED) {
-                        registeredEvents.remove(registeredEvent);
-                    }
+            for (EventListener<? extends Event> eventListener : eventListeners) {
+                @SuppressWarnings("unchecked")
+                EventListener<T> listener = (EventListener<T>) eventListener;
+                EventListener.Result result = listener.run(event);
+                if (result == EventListener.Result.EXPIRED) {
+                    listeners.get(event.getClass()).remove(eventListener);
                 }
             }
             return event;
-        });
+        }, ForkJoinPool.commonPool());
     }
 
 }
