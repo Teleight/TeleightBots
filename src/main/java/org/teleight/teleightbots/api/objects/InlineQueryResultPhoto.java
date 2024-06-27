4f2c46f5f79d2b795a11a4112717b9f695e0fcb5
Stredox02 <29194230+Stredox02@users.noreply.github.com>
2024-06-27 20:40:59 +0200

    Added builder for InlineQueryResultPhoto

--- src/main/java/org/teleight/teleightbots/api/objects/InlineQueryResultPhoto.java
+++ src/main/java/org/teleight/teleightbots/api/objects/InlineQueryResultPhoto.java
@@ -55,9 +55,87 @@ public record InlineQueryResultPhoto(
         InputMessageContent inputMessageContent
 ) implements InlineQueryResult {
 
-    @Override
-    public InlineQueryResultType type() {
-        return InlineQueryResultType.PHOTO;
-    }
+        public static @NotNull Builder ofBuilder(@NotNull String id, @NotNull String photoUrl, @NotNull String thumbnailUrl) {
+                return new Builder(id, photoUrl, thumbnailUrl);
+        }
+
+        @Override
+        public InlineQueryResultType type() {
+                return InlineQueryResultType.PHOTO;
+        }
+
+        public static final class Builder {
+                private String id;
+                private String photoUrl;
+                private String thumbnailUrl;
+                private int photoWidth;
+                private int photoHeight;
+                private String title;
+                private String description;
+                private String caption;
+                private ParseMode parseMode;
+                private MessageEntity[] captionEntities;
+                private boolean showCaptionAboveMedia;
+                private ReplyKeyboard replyMarkup;
+                private InputMessageContent inputMessageContent;
+
+                public Builder(@NotNull String id, @NotNull String photoUrl, @NotNull String thumbnailUrl) {
+                        this.id = id;
+                }
+
+                public @NotNull Builder photoWidth(int photoWidth) {
+                        this.photoWidth = photoWidth;
+                        return this;
+                }
+
+                public @NotNull Builder photoHeight(int photoHeight) {
+                        this.photoHeight = photoHeight;
+                        return this;
+                }
+
+                public @NotNull Builder title(@Nullable String title) {
+                        this.title = title;
+                        return this;
+                }
+
+                public @NotNull Builder description(@Nullable String description) {
+                        this.description = description;
+                        return this;
+                }
+
+                public @NotNull Builder caption(@Nullable String caption) {
+                        this.caption = caption;
+                        return this;
+                }
+
+                public @NotNull Builder parseMode(@Nullable ParseMode parseMode) {
+                        this.parseMode = parseMode;
+                        return this;
+                }
+
+                public @NotNull Builder captionEntities(@Nullable MessageEntity[] captionEntities) {
+                        this.captionEntities = captionEntities;
+                        return this;
+                }
+
+                public @NotNull Builder showCaptionAboveMedia(boolean showCaptionAboveMedia) {
+                        this.showCaptionAboveMedia = showCaptionAboveMedia;
+                        return this;
+                }
+
+                public @NotNull Builder replyMarkup(@Nullable ReplyKeyboard replyMarkup) {
+                        this.replyMarkup = replyMarkup;
+                        return this;
+                }
+
+                public @NotNull Builder inputMessageContent(@Nullable InputMessageContent inputMessageContent) {
+                        this.inputMessageContent = inputMessageContent;
+                        return this;
+                }
+
+                public @NotNull InlineQueryResultPhoto build() {
+                        return new InlineQueryResultPhoto(id, photoUrl, thumbnailUrl, photoWidth, photoHeight, title, description, caption, parseMode, captionEntities, showCaptionAboveMedia, replyMarkup, inputMessageContent);
+                }
+        }
 
 }
