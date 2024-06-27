5c1018ebd4a367f269fe14ba40cba9186b4fe7be
Stredox02 <29194230+Stredox02@users.noreply.github.com>
2024-06-27 20:31:39 +0200

    Added utility methods

--- src/main/java/org/teleight/teleightbots/event/user/UserInlineQueryReceivedEvent.java
+++ src/main/java/org/teleight/teleightbots/event/user/UserInlineQueryReceivedEvent.java
@@ -1,7 +1,9 @@
 package org.teleight.teleightbots.event.user;
 
 import org.jetbrains.annotations.NotNull;
+import org.teleight.teleightbots.api.objects.InlineQuery;
 import org.teleight.teleightbots.api.objects.Update;
+import org.teleight.teleightbots.api.objects.User;
 import org.teleight.teleightbots.bot.TelegramBot;
 import org.teleight.teleightbots.event.trait.Event;
 
@@ -9,5 +11,13 @@ public record UserInlineQueryReceivedEvent(
         @NotNull TelegramBot bot,
         @NotNull Update update
 ) implements Event {
+
+    public @NotNull User user() {
+        return update().inlineQuery().from();
+    }
+
+    public @NotNull InlineQuery inlineQuery() {
+        return update().inlineQuery();
+    }
 
 }
