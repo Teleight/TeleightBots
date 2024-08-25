67c6414f6a8c4987481c1da17979c0d0d3a60240
firo <56583784+literallyfiro@users.noreply.github.com>
2024-08-25 16:39:16 +0200

    Fix stack overflow error when deserializing message

--- src/main/java/org/teleight/teleightbots/api/objects/MaybeInaccessibleMessage.java
+++ src/main/java/org/teleight/teleightbots/api/objects/MaybeInaccessibleMessage.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.teleight.teleightbots.api.ApiResult;
 import org.teleight.teleightbots.api.serialization.deserializers.MaybeInaccessibleMessageDeserializer;
 
-@JsonDeserialize(using = MaybeInaccessibleMessageDeserializer.class)
+@JsonDeserialize(contentUsing = MaybeInaccessibleMessageDeserializer.class)
 public sealed interface MaybeInaccessibleMessage extends ApiResult permits
         Message,
         InaccessibleMessage {
