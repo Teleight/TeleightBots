e9dc7ab6282444ec3a8842ecbf67e21071314f8a
firo <56583784+literallyfiro@users.noreply.github.com>
2024-08-25 13:28:55 +0200

    Refactor

--- src/main/java/org/teleight/teleightbots/api/objects/ResponseParameters.java
+++ src/main/java/org/teleight/teleightbots/api/objects/ResponseParameters.java
@@ -5,7 +5,7 @@ import org.teleight.teleightbots.api.ApiResult;
 
 public record ResponseParameters(
         @JsonProperty("migrate_to_chat_id")
-        Long migrateToChatId,
+        long migrateToChatId,
 
         @JsonProperty("retry_after")
         int retryAfter
