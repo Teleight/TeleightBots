02ecd9c0e18f56fa17b2bdb3e552833260a3f816
Stredox02 <29194230+Stredox02@users.noreply.github.com>
2024-09-05 21:05:21 +0200

    Common fields exposed in `MaybeInaccessibleMessage`

--- src/main/java/org/teleight/teleightbots/api/objects/MaybeInaccessibleMessage.java
+++ src/main/java/org/teleight/teleightbots/api/objects/MaybeInaccessibleMessage.java
@@ -1,8 +1,18 @@
 package org.teleight.teleightbots.api.objects;
 
+import org.jetbrains.annotations.NotNull;
 import org.teleight.teleightbots.api.ApiResult;
+
+import java.util.Date;
 
 public sealed interface MaybeInaccessibleMessage extends ApiResult permits
         Message,
         InaccessibleMessage {
+
+    int messageId();
+
+    @NotNull Chat chat();
+
+    @NotNull Date date();
+
 }
